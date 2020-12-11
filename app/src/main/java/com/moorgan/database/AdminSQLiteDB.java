7f09593cc7dede39c24c10480761b85f7c9a6915
David Alejandro Nuñez Rangel <david2906yt@gmail.com>
2020-12-10 22:54:35 -0500

    Insert user and wallet query

--- app/src/main/java/com/moorgan/database/AdminSQLiteDB.java
+++ app/src/main/java/com/moorgan/database/AdminSQLiteDB.java
@@ -10,11 +10,14 @@ import androidx.annotation.Nullable;
 
 public class AdminSQLiteDB extends SQLiteOpenHelper {
 
+    //
     public static final int DB_DEFAULT_VERSION = 1;
     //Name of the data base
     public static final String DB_NAME = "";
     //
-    public static final String DB_TABLE_NAME_ = "";
+    public static final String DB_TABLE_NAME_USER = "user";
+    //
+    public static final String DB_TABLE_NAME_WALLET = "wallet";
 
     /**
      * Class constructor
@@ -45,7 +48,20 @@ public class AdminSQLiteDB extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL("CREATE TABLE user()");
+        db.execSQL("CREATE TABLE user(" +
+                "use_id integer PRIMARY KEY AUTOINCREMENT," +
+                "use_name varchar(30) NOT NULL," +
+                "use_last_name varchar(30) NOT NULL," +
+                "use_birthdate date NOT NULL DEFAULT '00/00/2000'," +
+                "use_career varchar(30) NOT NULL DEFAULT 'unknow'," +
+                "use_wallet integer NOT NULL," +
+                "FOREIGN KEY (use_wallet)" +
+                "   REFERENCES wallet (wal_id))");
+
+
+        db.execSQL("CREATE TABLE wallet(" +
+                "wal_id integer PRIMARY KEY AUTOINCREMENT," +
+                "wal_balance integer NOT NULL DEFAULT 0)");
 
     }
 
